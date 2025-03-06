af8350313d0375bf9f313fd894317e6b9b121d53
Ashwin-Selvaraj <ashwin240899@gmail.com>
2025-03-06 13:00:15 +0530

    added test scripts

--- tests/MinimalReproducibleExample.spec.ts
+++ tests/MinimalReproducibleExample.spec.ts
@@ -1,7 +1,8 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { toNano } from '@ton/core';
+import { Address, toNano, beginCell } from '@ton/core';
 import { MinimalReproducibleExample } from '../wrappers/MinimalReproducibleExample';
 import '@ton/test-utils';
+import { log } from 'node:console';
 
 describe('MinimalReproducibleExample', () => {
     let blockchain: Blockchain;
@@ -13,8 +14,7 @@ describe('MinimalReproducibleExample', () => {
 
         minimalReproducibleExample = blockchain.openContract(await MinimalReproducibleExample.fromInit());
 
-        deployer = await blockchain.treasury('deployer');
-
+        deployer = await blockchain.treasury('deployer');  
         const deployResult = await minimalReproducibleExample.send(
             deployer.getSender(),
             {
@@ -25,7 +25,7 @@ describe('MinimalReproducibleExample', () => {
                 queryId: 0n,
             }
         );
-
+        
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
             to: minimalReproducibleExample.address,
@@ -37,5 +37,29 @@ describe('MinimalReproducibleExample', () => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and minimalReproducibleExample are ready to use
+    });
+
+    it('should verify signature and mint', async () => {
+        const signatureResult = await minimalReproducibleExample.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'VerifySignatureMint',
+                userAddress: Address.parse("0QCfwXGx7kkNOCoE8QVOE5eYBMyNpcxvbMgEJ2UrDUM1TaOn"),
+                reward: 69000000000000000000n,
+                signature: beginCell()
+                    .storeBuffer(Buffer.from('e44b470b6e9c96e76c668ab20b6009efe310a3b27425c54a3ae3bb4d1ce281470d9eecb1b6dcee8c88cc32e3da76d716d3eb2bd7fe563eec4c51fbc559ed4a0e', 'hex'))
+                    .endCell()
+                    .beginParse()
+            }
+        );
+
+        expect(signatureResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: minimalReproducibleExample.address,
+            success: true,
+        });
     });
 });
